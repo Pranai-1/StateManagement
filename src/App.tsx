5b402ced54d001a9d53175899606bbf33b6823c7
Pranai-1 <reddypranai2017@gmail.com>
2023-12-20 11:46:26 +0530

    Initial

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Landing from './landing';
+
+function App() {
+  return (
+   <Router  >
+    <Routes>
+      <Route path="/" element={<Landing/>}/>
+    </Routes>
+   </Router>
+  );
+}
+
+export default App;
